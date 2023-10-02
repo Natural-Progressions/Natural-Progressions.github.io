5e5d8f16e0eed3a701a186d92030f0abe5fa154e
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-01 22:07:58 -0500

    Generate changelog entries from json

--- js/index.js
+++ js/index.js
@@ -1,11 +1,23 @@
 $(function () {
-    var j = [];
+    var logEntries = [];
     $.ajax({
         type: 'GET',
         url: "/json/changelog.json",
         dataType: 'json',
-        success: function(data) { j = data;},
+        success: function(data) { logEntries = data;},
         async: false
     });
-    console.log(j);
-  });
+    
+    for (let entryName in logEntries) {
+        let activities = logEntries[entryName]["Activities"];
+
+        let unorderedList = $("<ul>");
+
+        for (let index in activities) {
+            unorderedList.append($("<li>").text(activities[index]));
+        }
+
+        $('#changelog-sections').append( $("<h2>").addClass("text-center").addClass("py-2").addClass("bg-warning-subtle").text(entryName));
+        $('#changelog-sections').append(unorderedList);
+    }
+});
