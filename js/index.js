9cc05d50bfa82d89da9687de26ba966a9b45c174
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-01 23:06:45 -0500

    Maybe this one will work?

--- js/index.js
+++ js/index.js
@@ -21,23 +21,27 @@ $(function () {
     //     $('#changelog-sections').append(unorderedList);
     // }
 
-    fetch("/json/changelog.json")
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`HTTP error, status = ${response.status}`);
-            }
-            return response.json();
-        })
-        .then((data) => {
-            for (const entry of data.entries) {
-                $('#changelog-sections').append( $("<h2>").addClass("text-center").addClass("py-2").addClass("bg-warning-subtle").text(entry.Name));
-                let unorderedList = $("<ul>");
+    /* Entry definition:
+     *      Name: String
+     *      Activities: List of String 
+     */
+    fetch("/json/changelog.json").then((response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error, status = ${response.status}`);
+        }
+        return response.json();
+    }).then((data) => {
+        for (const entry of data.entries) {
+            $('#changelog-sections').append( $("<h2>").addClass("text-center").addClass("py-2").addClass("bg-warning-subtle").text(entry.Name));
+            let unorderedList = $("<ul>");
 
-                for (const description of entry.Activities) {
-                    unorderedList.append($("<li>").text(description));
-                }
-
-                $('#changelog-sections').append(unorderedList);
+            for (const description of entry.Activities) {
+                unorderedList.append($("<li>").text(description));
             }
-        })
+
+            $('#changelog-sections').append(unorderedList);
+        }
+    })
+
+    $.getScript( "/js/toc.js" );
 });
