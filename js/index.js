10f41ccea36d6a86d256836f47cc09700600e87b
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-01 23:10:41 -0500

    That last one worked. Clearing up the old crud

--- js/index.js
+++ js/index.js
@@ -1,26 +1,4 @@
 $(function () {
-    // var logEntries = [];
-    // $.ajax({
-    //     type: 'GET',
-    //     url: "/json/changelog.json",
-    //     dataType: 'json',
-    //     success: function(data) { logEntries = data;},
-    //     async: false
-    // });
-    
-    // for (let entryName in logEntries) {
-    //     let activities = logEntries[entryName]["Activities"];
-
-    //     let unorderedList = $("<ul>");
-
-    //     for (let index in activities) {
-    //         unorderedList.append($("<li>").text(activities[index]));
-    //     }
-
-    //     $('#changelog-sections').append( $("<h2>").addClass("text-center").addClass("py-2").addClass("bg-warning-subtle").text(entryName));
-    //     $('#changelog-sections').append(unorderedList);
-    // }
-
     /* Entry definition:
      *      Name: String
      *      Activities: List of String 
