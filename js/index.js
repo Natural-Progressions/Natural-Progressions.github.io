e4391b822d38a6031de00de8af6638dbe620e507
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-01 22:24:03 -0500

    Will this work as intended?

--- js/index.js
+++ js/index.js
@@ -1,43 +1,43 @@
-// $(function () {
-//     var logEntries = [];
-//     $.ajax({
-//         type: 'GET',
-//         url: "/json/changelog.json",
-//         dataType: 'json',
-//         success: function(data) { logEntries = data;},
-//         async: false
-//     });
+$(function () {
+    // var logEntries = [];
+    // $.ajax({
+    //     type: 'GET',
+    //     url: "/json/changelog.json",
+    //     dataType: 'json',
+    //     success: function(data) { logEntries = data;},
+    //     async: false
+    // });
     
-//     for (let entryName in logEntries) {
-//         let activities = logEntries[entryName]["Activities"];
+    // for (let entryName in logEntries) {
+    //     let activities = logEntries[entryName]["Activities"];
 
-//         let unorderedList = $("<ul>");
+    //     let unorderedList = $("<ul>");
 
-//         for (let index in activities) {
-//             unorderedList.append($("<li>").text(activities[index]));
-//         }
+    //     for (let index in activities) {
+    //         unorderedList.append($("<li>").text(activities[index]));
+    //     }
 
-//         $('#changelog-sections').append( $("<h2>").addClass("text-center").addClass("py-2").addClass("bg-warning-subtle").text(entryName));
-//         $('#changelog-sections').append(unorderedList);
-//     }
+    //     $('#changelog-sections').append( $("<h2>").addClass("text-center").addClass("py-2").addClass("bg-warning-subtle").text(entryName));
+    //     $('#changelog-sections').append(unorderedList);
+    // }
 
-//     fetch("/json/changelog.json")
-//       .then((response) => {
-//         if (!response.ok) {
-//           throw new Error(`HTTP error, status = ${response.status}`);
-//         }
-//         return response.json();
-//       })
-//       .then((data) => {
-//         for (const entry of data.entries) {
-//             $('#changelog-sections').append( $("<h2>").addClass("text-center").addClass("py-2").addClass("bg-warning-subtle").text(entry.Name));
-//             let unorderedList = $("<ul>");
+    fetch("/json/changelog.json")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error(`HTTP error, status = ${response.status}`);
+        }
+        return response.json();
+      })
+      .then((data) => {
+        for (const entry of data.entries) {
+            $('#changelog-sections').append( $("<h2>").addClass("text-center").addClass("py-2").addClass("bg-warning-subtle").text(entry.Name));
+            let unorderedList = $("<ul>");
 
-//             for (const description of entry.Activities) {
-//                 unorderedList.append($("<li>").text(description));
-//             }
+            for (const description of entry.Activities) {
+                unorderedList.append($("<li>").text(description));
+            }
 
-//             $('#changelog-sections').append(unorderedList);
-//         }
-//       })
-// });
+            $('#changelog-sections').append(unorderedList);
+        }
+      })
+});
