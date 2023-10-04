c64b16b381a7ad87ec021898360ee7acb718e031
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-03 22:29:40 -0500

    Does this look good?

--- js/navbar.js
+++ js/navbar.js
@@ -7,16 +7,11 @@ $(function () {
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="/core_rules.html">Core Rules</a>
-                </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Core Rules</a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><hr class="dropdown-divider"></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item" href="/core_rules.html">Assumptions and Terminology</a></li>
+                        <li><a class="dropdown-item" href="/skills.html">Skills</a></li>
                     </ul>
                 </li>
             </ul>
