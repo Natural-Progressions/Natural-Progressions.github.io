864030e5b125801b7ae58ab77a0f3057de06b6ee
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-04 21:02:59 -0500

    Change load time

--- js/skills.js
+++ js/skills.js
@@ -1,6 +1,6 @@
 async function redrawTOC() {
 
-    await delay(1000);
+    await delay(500);
     /* Set the skip attribute on all items that normally would get picked up */
     $('#skills-tabContent').find('h1, h2').attr('data-toc-skip','');
 
