9a9badf2f3e2760e6f477439fea8ba8b948e7ed5
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-04 22:49:35 -0500

    save dayNightMode

--- js/navbar.js
+++ js/navbar.js
@@ -31,12 +31,14 @@ async function toggleDayNightMode() {
     $("#day-night-mode-icon").remove();
 
     if ($("html").attr("data-bs-theme") == 'dark') {
+        localStorage.setItem("dayNightMode", "light");
         $("html").attr("data-bs-theme", "light");
 
         $("#day-night-mode-div").prepend(`<svg id="day-night-mode-icon" xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-brightness-high-fill" viewBox="0 0 16 16">
         <path d="M12 8a4 4 0 1 1-8 0 4 4 0 0 1 8 0zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/>
       </svg>`)
     } else {
+        localStorage.setItem("dayNightMode", "dark");
         $("html").attr("data-bs-theme", "dark");
 
         $("#day-night-mode-div").prepend(`<svg id="day-night-mode-icon" xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-moon-stars-fill" viewBox="0 0 16 16">
@@ -45,3 +47,9 @@ async function toggleDayNightMode() {
       </svg>`)
     }
 }
+
+$(function () {
+    if (localStorage.getItem("dayNightMode") == null) {
+        localStorage.setItem("dayNightMode", "light");
+    }
+});
