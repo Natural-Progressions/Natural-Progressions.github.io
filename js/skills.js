81dc3182772c99336519deb20893815ed8ad7239
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-04 21:09:56 -0500

    Activate the object

--- js/skills.js
+++ js/skills.js
@@ -1,16 +1,20 @@
 async function redrawTOC() {
 
-    await delay(250);
+    await delay(100);
     /* Set the skip attribute on all items that normally would get picked up */
     $('#skills-tabContent').find('h1, h2').attr('data-toc-skip','');
 
     /* Remove the skil attribute from the items currently being shown */
     $('#skills-tabContent').find('.show').find('h1, h2').removeAttr('data-toc-skip','');
 
+    /* Delete the existing table of contents */
     $("#toc").empty();
 
-    /* Rerun the toc script */
+    /* Rerun the toc script, regenerating the table of contents */
     $.getScript( "/js/toc.js" );
+
+    /* When run this way, the first object doesn't get set to active. Set it to active */
+    $("#toc").find('a').first().addClass('active');
 }
 
 function delay(milliseconds){
