46614ecc394eba89f542d899fe66204e91f555bc
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-04 22:19:03 -0500

    Switch test

--- js/navbar.js
+++ js/navbar.js
@@ -15,6 +15,10 @@ $(function () {
                     </ul>
                 </li>
             </ul>
+            <div class="form-check form-switch text-right">
+                <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                <label class="form-check-label" for="flexSwitchCheckDefault">Default switch checkbox input</label>
+            </div>
         </div>
     </div>
 </nav>`);
