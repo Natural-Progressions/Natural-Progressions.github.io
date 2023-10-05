fb81c49772c9d613d8515735b6f095102d85db7e
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-04 20:45:12 -0500

    Test resetting table of contents

---
+++ js/skills.js
@@ -0,0 +1,10 @@
+function redrawTOC() {
+    /* Set the skip attribute on all items that normally would get picked up */
+    $('#skills-tabContent').find('h1, h2').attr('data-toc-skip','');
+
+    /* Remove the skil attribute from the items currently being shown */
+    $('#skills-tabContent').find('.show').find('h1, h2').removeAttr('data-toc-skip','');
+
+    /* Rerun the toc script */
+    $.getScript( "/js/toc.js" );
+}
