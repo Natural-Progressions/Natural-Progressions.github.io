0008e94797f0791363b5ad4d340b40703ee9c533
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-04 20:55:03 -0500

    Remove children

--- js/skills.js
+++ js/skills.js
@@ -5,6 +5,8 @@ function redrawTOC() {
     /* Remove the skil attribute from the items currently being shown */
     $('#skills-tabContent').find('.show').find('h1, h2').removeAttr('data-toc-skip','');
 
+    $("#toc").empty();
+
     /* Rerun the toc script */
     $.getScript( "/js/toc.js" );
 }
