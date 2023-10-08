61800cf1874edbeb15fee886c734a210fff4014d
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-10-07 23:09:07 -0500

    More fix

--- js/toc.js
+++ js/toc.js
@@ -2,7 +2,7 @@ $(function () {
     var navSelector = "#toc";
     Toc.init({
         $nav: $("#toc"), 
-        $scope: $("h1, h2")
+        $scope: $("h1, h2, h3, h4, h5, h6")
     })
     $("body").scrollspy({
       target: navSelector,
