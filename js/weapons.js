6c1128da58cd9e4174d28639571df0b025470a42
SirRunner <33076111+SirRunner@users.noreply.github.com>
2023-11-06 21:19:25 -0600

    add column searches

--- js/weapons.js
+++ js/weapons.js
@@ -48,7 +48,26 @@ $(function () {
                 [-1, 10, 25, 50, 100],
                 ['All', 10, 25, 50, 100]
             ],
-            paging: false
+            initComplete: function () {
+                this.api()
+                    .columns()
+                    .every(function () {
+                        let column = this;
+                        let title = column.footer().textContent;
+         
+                        // Create input element
+                        let input = document.createElement('input');
+                        input.placeholder = title;
+                        column.footer().replaceChildren(input);
+         
+                        // Event listener for user input
+                        input.addEventListener('keyup', () => {
+                            if (column.search() !== this.value) {
+                                column.search(input.value).draw();
+                            }
+                        });
+                    });
+            }
         });
 
         DataTable.ext.search.push(function (settings, data, dataIndex) {
